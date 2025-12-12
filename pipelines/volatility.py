6f1d3526eb7e2294a38324cab78e91c5f5a04395
Mihir Joshi <mnj2122@columbia.edu>
2025-12-12 15:23:59 -0500

    figuring out how to clean yf pull into proper format for processing

--- pipelines/volatility.py
+++ pipelines/volatility.py
@@ -1,16 +1,22 @@
+import yfinance as yf
+import pandas as pd
+import datetime as dt
 import numpy as np
 from .base import Pipeline
 
 class VolatilityPipeline(Pipeline):
-    def __init__(self, mu=-3.0, phi=0.95, sigma=0.15, seed=43):
-        self.mu = mu
-        self.phi = phi
-        self.sigma = sigma
-        self.rng = np.random.default_rng(seed)
-        self.last_log_val = self.mu
+    def __init__(self):
+        pass
 
-    def get_latest_data(self) -> dict:
-        noise = self.rng.normal(0.0, self.sigma)
-        new_log_val = self.mu + self.phi * (self.last_log_val - self.mu) + noise
-        self.last_log_val = new_log_val
-        return {"har_rv": float(np.exp(new_log_val))}
+    def get_latest_data(self):
+        start_date = dt.datetime.today() - dt.timedelta(days=59)
+        curr_data = yf.download("SPY", period="1y", interval="5m", start=start_date)
+        curr_data = curr_data[curr_data['Volume'] != 0]
+        curr_data = curr_data.dropna()
+        return curr_data
+    
+    def get_har_rv(self, data: pd.DataFrame) -> float:
+        curr_data = self.get_latest_data()
+        har_rv = np.sqrt(np.mean(np.square(curr_data['Close'] - curr_data['Close'].shift(1))))
+        
+

fde4dbf5b2594df8129be95b97a0030d69213cb2
Mihir Joshi <mnj2122@columbia.edu>
2025-12-12 16:58:57 -0500

    fix internal pipeline

--- pipelines/volatility.py
+++ pipelines/volatility.py
@@ -8,7 +8,8 @@ class VolatilityPipeline(Pipeline):
     def __init__(self):
         pass
 
-    def get_latest_data(self):
+    def _get_latest_data(self) -> pd.DataFrame:
+
         # pull initial data from yfinance
         start_date = dt.datetime.today() - dt.timedelta(days=59)
         curr_data = yf.download("SPY", period="1y", interval="5m", start=start_date)
@@ -24,12 +25,9 @@ class VolatilityPipeline(Pipeline):
 
         return curr_data_cleaned
     
-    def har_rv_calculation(self):
+    def __rv_calculation(self, df: pd.DataFrame) -> pd.DataFrame:
         
-        # pull latest data
-        df = self.get_latest_data() 
-
-        # period calculation
+        # calculate period
         df["D"] = df.index.date
         n_periods = df.pivot_table(index = ["D"], aggfunc = 'size').values
         df.loc[df["D"] != df["D"].shift(), "Per"] = n_periods
@@ -38,18 +36,18 @@ class VolatilityPipeline(Pipeline):
         # calculate daily returns
         df["Ret"] = np.where(df["D"] == df["D"].shift(), ( (df["close"]-df["close"].shift()) * 1/df["Per"] ) **2, np.nan)
         
-        # calculate daily RV
+        # calculate realized variance daily
         rv = df.groupby("D")["Ret"].agg(np.sum).to_frame()
         rv.columns = ["RV_daily"]
         rv["RV_daily"] = np.sqrt(rv["RV_daily"])
-        
-        # find max RV date
-        date = str(rv["RV_daily"].idxmax())
-        close_price = df["close"].loc[date]
-        
-        return close_price
 
-    def get_har_rv(self, data: pd.DataFrame) -> float:
-        pass
+        # Compute weekly and monthly RV.  
+        rv["RV_weekly"] = rv["RV_daily"].rolling(5).mean()
+        rv["RV_monthly"] = rv["RV_daily"].rolling(21).mean()
+        rv.dropna(inplace = True)
+
+        return rv
+    
+    def __get_har_rv(self, data: pd.DataFrame) -> float:
         
 

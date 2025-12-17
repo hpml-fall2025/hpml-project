b0973e11d2b72fbc15417c34182a3339ae9992e0
Shriya Mahakala <55512194+shriyamahakala@users.noreply.github.com>
2025-12-17 10:57:12 -0500

    Fixed quickstart predict for hourly predictions

--- predict.py
+++ predict.py
@@ -2,14 +2,13 @@ import pipelines.weighting
 import datetime 
 
 
-query_date = input("Enter the date (YYYY-MM-DD) to query volatility prediction: ")
+query_date = input("Enter the time (YYYY-MM-DD HH) to query volatility prediction: ")
 
 
 try:
-    date_obj = datetime.datetime.strptime(query_date, "%Y-%m-%d").date()
-    date_obj = date_obj.date()
+    time_obj = datetime.datetime.strptime(query_date, "%Y-%m-%d %H")
 except ValueError:
     print("Invalid date format.")
     exit(1)
 
-print("Predicted Volatility for", date_obj, "is:", pipelines.weighting.predict_weighted_vol(date_obj))
+print("Predicted Volatility for", date_obj, "is:", pipelines.weighting.predict_weighted_vol(time_obj))
